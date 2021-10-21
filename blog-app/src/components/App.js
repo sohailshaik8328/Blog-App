305fc34b7ad47b33c63842f266233ec9756923ef
sohailshaik8328 <sohailshaik765@gmail.com>
2021-10-21 11:58:20 +0530

    adf

--- blog-app/src/components/App.js
+++ blog-app/src/components/App.js
@@ -6,32 +6,134 @@ import Signin from './Signin';
 import SignUp from './SignUp';
 import SingleArticle from './SingleArticle';
 import NoMatch from './NoMatch';
+import React from 'react';
+import { BaseUrl, localStorageKey } from './utils/constant';
+import Loader from './Loader';
 
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLogged : false,
+      user : null,
+      isVerified : true
+    }
+  }
+  logout = () => {
+    this.setState({
+      isLogged : false,
+      user : null,
+      isVerified : false
+    })
+    localStorage.clear();
+  }
+
+
+  updateUser = (loggedUser) => {
+    this.setState({
+      isLogged : true,
+      user : loggedUser,
+      isVerified : false
+    })
+    localStorage.getItem(localStorageKey, loggedUser.token)
+  }
+
+  componentDidMount() {
+    let url = BaseUrl + "user";
+    let key = localStorage[localStorageKey];
+    if(key) {
+      fetch(url, {
+        method : "GET",
+        headers : {authorization : `Token ${key}`},
+      })
+      .then(res => {
+        if(res.ok) {
+          return res.json()
+        }
+        return res.json().then(({errors}) => {
+          return Promise.reject(errors)
+        })
+      })
+      .then((user) => {
+        this.updateUser(user)
+      })
+      .catch(err => console.log(err))
+    } else {
+      this.setState({
+        isVerified : false
+      })
+    }
+  }
+
+ render() {
+   if(this.state.isVerified) {
+     return <Loader />
+    //  <img src="/images/login.gif" alt="login Gif" />
+   }
   return (
     <>
-      <Header />
-   
-      <Switch>
-        <Route path='/' exact >
-          <Home />
-        </Route>
-        <Route path="/signin" exact >
-            <Signin />
-        </Route>
-        <Route path="/signup" exact >
-            <SignUp />
-        </Route>
-        <Route path="/article/:slug" component={SingleArticle} exact/>
-
-        <Route path="*">
-          <NoMatch />
-        </Route>
-      </Switch>
+    <Header {...this.state} logout={this.logout} />
+      {
+        this.state.isLogged ? <Authenticated logout = {this.logout} /> : <UnAuthenticated />
+      }
     
     </>
   );
+ }
 }
+
+function UnAuthenticated(props) {
+  return (
+    <>
+    {/* <Header /> */}
+   <Switch>
+     <Route path='/' exact >
+       <Home />
+     </Route>
+     <Route path="/signin" exact >
+         <Signin />
+     </Route>
+     <Route path="/signup" exact >
+         <SignUp />
+     </Route>
+     <Route path="/article/:slug" component={SingleArticle} exact/>
+
+     <Route path="*">
+       <NoMatch />
+     </Route>
+   </Switch>
+    </>
+  )
+}
+
+function Authenticated(props) {
+  return (
+    <>
+  {/* <Header /> */}
+   <Switch>
+     <Route path='/' exact >
+       <Home />
+     </Route>
+     <Route>
+
+     </Route>
+     {/* <Route path="/signin" exact >
+         <Signin />
+     </Route> */}
+     {/* <Route path="/signup" exact >
+         <SignUp />
+     </Route> */}
+     <Route path="/article/:slug" component={SingleArticle} exact/>
+
+     <Route path="*">
+       <NoMatch />
+     </Route>
+   </Switch>
+    </>
+  )
+}
+
+
 
 export default App;
--- blog-app/src/components/utils/constant.js
+++ blog-app/src/components/utils/constant.js
@@ -1,9 +1,9 @@
 const BaseUrl = `https://mighty-oasis-08080.herokuapp.com/api/`;
 const  ArticlesUrl = BaseUrl + "articles";
-
 const TagsUrl = BaseUrl + "tags";
+const localStorageKey = "TokenKey";
 
 export {
-    BaseUrl, ArticlesUrl, TagsUrl
+    BaseUrl, ArticlesUrl, TagsUrl, localStorageKey
 }
 
