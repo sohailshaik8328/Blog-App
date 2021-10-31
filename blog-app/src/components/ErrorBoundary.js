788c09ab87a92a786b659f29c334dfbc605254f1
sohailshaik8328 <sohailshaik765@gmail.com>
2021-10-31 10:05:30 +0530

    Asdfa

--- blog-app/src/components/ErrorBoundary.js
+++ blog-app/src/components/ErrorBoundary.js
@@ -11,7 +11,7 @@ export default class ErrorBoundary extends Component {
 
     render() {
         if(this.state.hasError) {
-            return <h1>Something went wrong</h1>
+            return <h1>Something went wrong!</h1>
         }
         return (
             <div>
